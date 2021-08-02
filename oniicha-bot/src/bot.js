b1b80da047cfb798d38d38e5e4093d34c33cdb9d
Allysht <81968946+Allysht@users.noreply.github.com>
2021-08-02 04:33:17 +0300

    trying to resolve the .catch error

--- oniicha-bot/src/bot.js
+++ oniicha-bot/src/bot.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
+router.get("/emailfetch", authCheck, async (req, res, next) => {
+    try {
+    //listing messages in users mailbox 
+      let emailFetch = await gmaiLHelper.getEmails(req.user._doc.profile_id , '/messages', req.user.accessToken)
+      emailFetch = emailFetch.data
+      res.send(emailFetch)
+    } catch (err) {
+      next(err);
+    }
+  })
+
 const { Client } = require('discord.js');
 const client = new Client();
 
